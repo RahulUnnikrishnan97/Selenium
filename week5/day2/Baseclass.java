85134f51a4520de8ea674af726a8c1e3bc7648d3
RahulUnnikrishnan97 <168268760+RahulUnnikrishnan97@users.noreply.github.com>
2024-06-01 07:30:36 +0530

    Add files via upload

---
+++ week5/day2/Baseclass.java
@@ -0,0 +1,29 @@
+package week5.day2;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class Baseclass {
+	
+	public static ChromeDriver driver;
+	@Parameters({"url","username","password"})
+	@Test @BeforeMethod
+	public void setUp(String url, String uname, String pwd) {
+		//throw new RuntimeException();
+		
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get(url);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.id("username")).sendKeys(uname);
+		driver.findElement(By.id("password")).sendKeys(pwd);
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+	}
+
+}
---
+++ week5/day2/CreateLead1.java
@@ -0,0 +1,54 @@
+package week5.day2;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+
+public class CreateLead1 extends Baseclass {
+@Test //(dependsOnMethods = {"week5.day2.Baseclass.setUp"})
+	public void createLead () {
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TestLeaf");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Hari");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("R");
+		driver.findElement(By.name("submitButton")).click();
+		driver.close();
+}
+}
+
+//@Test //(dependsOnMethods = {"week5.day2.CreateLead1.createLead"})
+//public void deleteLead () throws InterruptedException {
+//
+//	driver.findElement(By.linkText("Leads")).click();
+//	driver.findElement(By.linkText("Find Leads")).click();
+//	driver.findElement(By.xpath("//span[text()='Phone']")).click();
+//	driver.findElement(By.xpath("//input[@name='phoneNumber']")).sendKeys("9");
+//	driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
+//	Thread.sleep(2000);
+//	String leadID = driver.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId']/a")).getText();
+//	driver.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId']/a")).click();
+//	driver.findElement(By.linkText("Delete")).click();
+//	driver.findElement(By.linkText("Find Leads")).click();
+//	driver.findElement(By.xpath("//input[@name='id']")).sendKeys(leadID);
+//	driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
+//	String text = driver.findElement(By.className("x-paging-info")).getText();
+//	if (text.equals("No records to display")) {
+//		System.out.println("Text matched");
+//	} else {
+//		System.out.println("Text not matched");
+//	}
+//}
+//
+//
+//}
+//
+//
+//
+
+
+
